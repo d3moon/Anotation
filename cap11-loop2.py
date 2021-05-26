b65270f4afa73509d349e11c3403a1a2da3957e3
João Victor <johnvict0r@ibm.com>
2021-05-26 00:50:32 -0300

    ff

---
+++ cap11-loop2.py
@@ -0,0 +1,7 @@
+#criação da variável com um valor inicial
+i = 0
+#enquanto a variável contadora não chegar ao limite
+while (i<10):
+    #para cada uma das repetições uma mensagem é exibida
+    print("Mais uma mensagem, com i valendo: {}".format(i))
+    i = i + 1
